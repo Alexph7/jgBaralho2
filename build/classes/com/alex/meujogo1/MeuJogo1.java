d8ab87bfdd46cddafc9b0677c8323087fc3e5bb6
alexph7 <alexph7.56@gmail.com>
2021-10-05 23:06:53 -0300

    up

---
+++ build/classes/com/alex/meujogo1/MeuJogo1.java
@@ -0,0 +1,20 @@
+package com.alex.meujogo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ph756
+ */
+public class MeuJogo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        JogoBaralho baralho = new JogoBaralho();
+        System.out.println(baralho.cartaAtual());
+        
+    }
+}
---
+++ src/com/alex/meujogo1/JogoBaralho.java
@@ -0,0 +1,27 @@
+package com.alex.meujogo1;
+
+import java.security.SecureRandom;
+
+/**
+ *
+ * @author ph756 --------
+ */
+public class JogoBaralho {
+
+    private final String[] numeros = {"Az", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    private final String[] nipes = {"Copas", "Ouros", "Paus", "Espadas"};
+    private final String carta[] = new String[52];
+    SecureRandom random = new SecureRandom();
+         
+    public JogoBaralho() {
+
+        for (int i = 0; i < carta.length; i++) {
+            this.carta[i] = this.numeros[random.nextInt(13)] + " de " + this.nipes[random.nextInt(4)];
+        }
+    }
+    public String cartaAtual(){
+        
+       String cartaAt = carta[random.nextInt(1)];
+        return cartaAt;
+    }
+}
